464f09df33c9a85b64b6048fdf70f0c9a35210f5
lucasheld <lucasheld@hotmail.de>
2022-12-04 18:28:06 +0100

    prepare client collection init

--- wg_ha_backend/app.py
+++ wg_ha_backend/app.py
@@ -1,9 +1,12 @@
+import os
+
+import yaml
 from celery import Celery
 from flask import Flask
 from flask_cors import CORS
-
-from config import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
 from pymongo import MongoClient
+
+from config import CELERY_BROKER_URL, CELERY_RESULT_BACKEND, ANSIBLE_PROJECT_PATH
 
 app = Flask(__name__)
 
@@ -16,6 +19,8 @@ celery.conf.update(app.config)
 
 client = MongoClient('mongo', 27017)
 db = client.flask_db
+
+# init collection server
 if not db.server.find_one({}):
     db.server.insert_one({
         "interface_ips": [
@@ -26,3 +31,13 @@ if not db.server.find_one({}):
         "public_key": "SPlAIzq4bkT3IxpFDnxfxACIaLoYMsv/WjxHTr6ZDR8=",
         "endpoint": "116.202.189.178:51820"
     })
+
+# init collection clients
+# if not db.clients.find_one({}):
+#     ansible_config_path = os.path.join(ANSIBLE_PROJECT_PATH, "group_vars", "all", "wireguard_peers")
+#     with open(ansible_config_path) as f:
+#         data = yaml.safe_load(f)
+#     peers = data.get("wireguard", {}).get("peers", [])
+#     for index, peer in enumerate(peers):
+#         # TODO: title and private_key missing
+#         db.clients.insert_one(peer)
